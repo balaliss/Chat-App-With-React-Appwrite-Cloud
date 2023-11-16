aa9ad60c3ee558abb3b1eb3563257be2b0caca0e
balaliss <balalis43@gmail.com>
2023-11-16 14:27:43 -0500

    updated appwriteConfig.js

--- chat-tut/src/appwriteConfig.js
+++ chat-tut/src/appwriteConfig.js
@@ -1,4 +1,9 @@
-import { Client } from 'appwrite';
+import { Client, Databases } from 'appwrite';
+
+export const PROJECT_ID = '6554dbf65c1b2983af87'
+export const DATABASE_ID = '6554dd2d71c9af211ede'
+export const COLLECTION_ID_MESSAGES = '6554dd433da6b09535a1'
+
 
 const client = new Client();
 
@@ -6,4 +11,6 @@ client
     .setEndpoint('https://cloud.appwrite.io/v1')
     .setProject('6554dbf65c1b2983af87');
 
-  export default client;
+    export const databases = new Databases(client)
+  
+    export default client;
