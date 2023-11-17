804fa0f03dae8a4d3ccd1405deebbbc3518afd8e
balaliss <balalis43@gmail.com>
2023-11-17 15:53:09 -0500

    Updated some code for pages

--- chat-tut/src/pages/Room.jsx
+++ chat-tut/src/pages/Room.jsx
@@ -2,9 +2,7 @@ import React, {useState, useEffect} from 'react'
 import client, { databases, DATABASE_ID, COLLECTION_ID_MESSAGES } from '../appwriteConfig'
 import { ID, Query, Permission, Role} from 'appwrite';
 import Header from '../components/Header';
-import { useAuth } from '../utils/AuthContext';
-import {Trash2} from 'react-feather'
-
+import { useAuth } from '../utils/AuthContext'import {Trash2} from 'react-feather'
 
 const Room = () => {
     const [messageBody, setMessageBody] = useState('')
